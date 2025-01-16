11c99d2bb9bc1d306433e86e965339107bba7e40
Arman Soudi <s0sarman@gmail.com>
2025-01-16 20:11:33 +0100

    remove unwanted code

--- src/index.ts
+++ src/index.ts
@@ -77,66 +77,6 @@ const run = async (): Promise<void> => {
   info(jobSummary);
   endGroup();
   setOutput('job-summary', jobSummary);
-
-  if (input.createMd) {
-    writeFileSync(`./${mdFile}`, jobSummary);
-  }
-
-  //   const configFileName = '_config.js';
-  //   // https://gist.github.com/danishcake/d045c867594d6be175cb394995c90e2c#file-readme-md
-  //   const config = `// A marked renderer for mermaid diagrams
-  // const renderer = {
-  //     code(code, infostring) {
-  //         if (infostring === 'mermaid'){
-  //             return \`<pre class="mermaid">$\{code}</pre>\`
-  //         }
-  //         return false
-  //     },
-  // };
-
-  // module.exports = {
-  //     marked_extensions: [{ renderer }],
-  //     script: [
-  //         { url: 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js' },
-  //         // Alternative to above: if you have no Internet access, you can also embed a local copy
-  //         // { content: require('fs').readFileSync('./node_modules/mermaid/dist/mermaid.js', 'utf-8') }
-  //         // For some reason, mermaid initialize doesn't render diagrams as it should. It's like it's missing
-  //         // the document.ready callback. Instead we can explicitly render the diagrams
-  //         { content: 'mermaid.initialize({ startOnLoad: false}); (async () => { await mermaid.run(); })();' }
-  //     ]
-  // };`;
-  // execSync(`npm i -g md-to-pdf`);
-  // writeFileSync(configFileName, config);
-  // execSync(`md-to-pdf --launch-options '{ "args": ["--no-sandbox"] }' --config-file ./${configFileName} ./${mdFile}`);
-  // info('PDF generated successfully');
-  // execSync(`md-to-pdf --launch-options '{ "args": ["--no-sandbox"] }' --config-file ./${configFileName} ./${mdFile} --as-html`);
-  // info('HTML generated successfully');
-  // unlinkSync(configFileName);
-
-  setOutput('job-summary-html', readFileSync(htmlFile, 'utf8'));
-
-  if (input.createPdfArtifact) {
-    const artifact = new DefaultArtifactClient()
-    await artifact.uploadArtifact(input.artifactName ? input.artifactName + '-pdf' : 'pdf', [pdfFile], '.')
-  }
-
-  if (input.createMdArtifact) {
-    const artifact = new DefaultArtifactClient()
-    await artifact.uploadArtifact(input.artifactName ? input.artifactName + '-md' : 'md', [mdFile], '.')
-  }
-
-  if (input.createHtmlArtifact) {
-    const artifact = new DefaultArtifactClient()
-    await artifact.uploadArtifact(input.artifactName ? input.artifactName + '-html' : 'html', [htmlFile], '.')
-  }
-
-  if (!input.createMd) unlinkSync(pdfFile);
-  if (!input.createPdf) unlinkSync(pdfFile);
-  if (!input.createHtml) unlinkSync(htmlFile);
-
-  setOutput('pdf-file', path.resolve(pdfFile));
-  setOutput('md-file', path.resolve(mdFile));
-  setOutput('html-file', path.resolve(htmlFile));
 };
 
 run();
